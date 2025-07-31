23c844c261ee7ca796adc785297c9da5f359ba4b
Can Yilmaz <223661918+pofhuduk@users.noreply.github.com>
2025-08-01 00:05:47 +0300

    feat: Add tv show search feature

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 from dotenv import load_dotenv
 import subprocess
 
-def search_movie(url:str, api:str, movie_name:str):
+def search(url:str, api:str, name:str):
     params = {
-            'query': movie_name,
+            'query': name,
             'api_key': api
             }
     r = requests.get(url, params=params).json()
@@ -13,8 +13,8 @@ def search_movie(url:str, api:str, movie_name:str):
     
     return results
 
-def get_movie(json:list, index:int):
-    movie = json[index]
+def get_movie(movie_list:list, index:int):
+    movie = movie_list[index]
 
     title = movie['title']
     overview = movie['overview']
@@ -29,11 +29,25 @@ def get_movie(json:list, index:int):
             'average_vote': vote_formatted
             }
 
-def main(api:str, url:str):
-    subprocess.call('clear')
-    movie_name = input('Movie Name:')
+def get_tv(tv_list:list, index:int):
+    show = tv_list[index]
+
+    name = show['name']
+    overview = show['overview']
+    first_air_date = show['first_air_date']
+    vote_average = show['vote_average']
+    vote_formatted = str(vote_average)[0:3]
     
-    results = search_movie(url=url, movie_name=movie_name, api=api)
+    return {
+            'name': name,
+            'overview': overview,
+            'first_air_date': first_air_date,
+            'average_vote': vote_formatted
+            }
+
+def run_movie_search(api:str, url:str):
+    movie_name = input('Movie Name:')    
+    results = search(url=url, name=movie_name, api=api)
 
     if (len(results) == 1):
         index = 0
@@ -44,12 +58,17 @@ def main(api:str, url:str):
         text = ''
         for index,obj in enumerate(results[0:10]):
             text += f'[{index + 1}] '+ obj['title'] + ' | ' + obj['release_date'] + '\n'
+        text += '[0] Exit\n'
         choice = input(text + 'Choose:')
+        
+        if (choice == '0'):
+            print('Goodbye!')
+            return 'exit'
         index = int(choice) - 1
+        
+    movie_data = get_movie(movie_list=results, index=index)
 
-    movie_data = get_movie(json=results, index=index)
-
-    print(f'''
+    return (f'''
         =========================
                MOVIE SEARCH
         =========================
@@ -60,6 +79,51 @@ def main(api:str, url:str):
         {movie_data['overview']}''')
 
 
+def run_tv_search(api:str, url:str):
+    tv_name = input('Show Name:')
+    results = search(url=url, name=tv_name, api=api)
+
+    if (len(results) == 1):
+        index = 0
+    elif (len(results) == 0):
+        print("No results found.")
+        return 0
+    else:
+        text = ''
+        for index,obj in enumerate(results[0:10]):
+            text += f'[{index + 1}] '+ obj['name'] + ' | ' + obj['first_air_date'] + '\n'
+        text += '[0] Exit\n'
+        choice = input(text + 'Choose:')
+        
+        if (choice == '0'):
+            print('Goodbye!')
+            return 'exit'
+        index = int(choice) - 1
+        
+    tv_data = get_tv(tv_list=results, index=index)
+
+    return (f'''
+        =========================
+                SHOW SEARCH
+        =========================
+        Movie Name: {tv_data['name']}
+        Release Date: {tv_data['first_air_date']}
+        iMDB: {tv_data['average_vote']}
+        Overview:
+        {tv_data['overview']}''')
+
+def main(api:str, mov_url:str, tv_url:str):
+    subprocess.call('clear')
+    x = input('1 - Movie Search | 2 - TV Show Search')
+    if (x == '1'):
+        result = run_movie_search(api=api, url=mov_url)
+    elif (x == '2'):
+        result = run_tv_search(api=api, url=tv_url)
+    else:
+        result = None
+
+    print(result)
+
 load_dotenv()
 API = os.getenv('API')
 
@@ -67,11 +131,13 @@ if not API:
     print('No API keys found.')
     exit()
 
-URL = 'https://api.themoviedb.org/3/search/movie'
+MOVIE_URL = 'https://api.themoviedb.org/3/search/movie'
+TV_URL = 'https://api.themoviedb.org/3/search/tv'
 
 while True:
     try:
-        main(api=API, url=URL)
+        main(api=API, mov_url=MOVIE_URL, tv_url=TV_URL)
     except (IndexError, ValueError):
         print("Invalid input. Please enter a number from the list.")
     input('Press ENTER to continue...')
+
